9b9c3752fbc3f133670486eaa626c6f4231092bf
git-nasu <nasu301130@icloud.com>
2025-04-03 21:51:35 +0900

    lus_ls error of undefined global vim  fixed

---
+++ lua/nasu00/core/autocmd.lua
@@ -0,0 +1,27 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- Add any additional autocmds here
+
+local autocmd = vim.api.nvim_create_autocmd --Create autocommand
+
+--Remove whitespace on save
+autocmd("BufWritePre", {
+	pattern = "*",
+	command = ":%s/\\s\\+$//e",
+})
+
+--Don't auto commenting on save
+
+autocmd("BufEnter", {
+	pattern = "*",
+	command = "set fo-=c fo-=r fo-=o", -- fo='formationoptions'
+})
+
+--Disable the concealing in some file formats
+--The default concealevel is 3 in LazyVim
+-- vim.api.nvim_create_autocmd({ "FileType" }, {
+--   pattern = { "json", "jsonc" },
+--   callback = function()
+--     vim.wo.conceallevel = 0
+--   end,
+-- })
